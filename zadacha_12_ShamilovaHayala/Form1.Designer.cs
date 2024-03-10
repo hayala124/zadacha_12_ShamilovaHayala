82f221f5678594560391316a3ef06da99921b766
hayala124 <elnur7803@gmail.com>
2024-03-10 21:44:20 +0300

    Добавьте файлы проекта.

---
+++ zadacha_12_ShamilovaHayala/Form1.Designer.cs
@@ -0,0 +1,84 @@
+﻿namespace zadacha_12_ShamilovaHayala
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.StartOverButton = new System.Windows.Forms.Button();
+            this.CheckButton = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // StartOverButton
+            // 
+            this.StartOverButton.BackColor = System.Drawing.Color.PowderBlue;
+            this.StartOverButton.FlatAppearance.BorderSize = 0;
+            this.StartOverButton.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.StartOverButton.Font = new System.Drawing.Font("MV Boli", 9F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.StartOverButton.Location = new System.Drawing.Point(614, 297);
+            this.StartOverButton.Name = "StartOverButton";
+            this.StartOverButton.Size = new System.Drawing.Size(168, 46);
+            this.StartOverButton.TabIndex = 3;
+            this.StartOverButton.Text = "Начать сначала";
+            this.StartOverButton.UseVisualStyleBackColor = false;
+            this.StartOverButton.Click += new System.EventHandler(this.StartOverButton_Click);
+            // 
+            // CheckButton
+            // 
+            this.CheckButton.BackColor = System.Drawing.Color.PowderBlue;
+            this.CheckButton.FlatAppearance.BorderSize = 0;
+            this.CheckButton.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.CheckButton.Font = new System.Drawing.Font("MV Boli", 9F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.CheckButton.Location = new System.Drawing.Point(614, 227);
+            this.CheckButton.Margin = new System.Windows.Forms.Padding(4);
+            this.CheckButton.Name = "CheckButton";
+            this.CheckButton.Size = new System.Drawing.Size(168, 46);
+            this.CheckButton.TabIndex = 2;
+            this.CheckButton.Text = "Проверить";
+            this.CheckButton.UseVisualStyleBackColor = false;
+            this.CheckButton.Click += new System.EventHandler(this.CheckButton_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(809, 554);
+            this.Controls.Add(this.StartOverButton);
+            this.Controls.Add(this.CheckButton);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Решение судоку";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button StartOverButton;
+        private System.Windows.Forms.Button CheckButton;
+    }
+}
+
---
+++ zadacha_12_ShamilovaHayala/Form1.cs
@@ -0,0 +1,261 @@
+﻿using System;
+using System.Drawing;
+using System.Linq;
+using System.Windows.Forms;
+
+namespace zadacha_12_ShamilovaHayala
+{
+    public partial class Form1 : Form
+    {
+        const int n = 3;
+        const int sizeButton = 50;
+        public int[,] map = new int[n * n, n * n];
+        public Button[,] buttons = new Button[n * n, n * n];
+        public Form1()
+        {
+            InitializeComponent();
+            GenerateMap();
+        }
+        private void GenerateMap()
+        {
+            for (int i = 0; i < n * n; i++)
+            {
+                for (int j = 0; j < n * n; j++)
+                {
+                    map[i, j] = (i * n + i / n + j) % (n * n) + 1;
+                    buttons[i, j] = new Button();
+                }
+            }
+
+            Random random = new Random();
+            for (int i = 0; i < 40; i++)
+            {
+                ShuffleMap(random.Next(0, 5));
+            }
+            CreateMap();
+            HideCells();
+        }
+        public void CreateMap()
+        {
+            for (int i = 0; i < n * n; i++)
+            {
+                for (int j = 0; j < n * n; j++)
+                {
+                    Button button = new Button();
+                    buttons[i, j] = button;
+                    button.Size = new Size(sizeButton, sizeButton);
+                    button.Text = map[i, j].ToString();
+                    button.Click += OnCellPressed;
+                    button.Location = new Point(j * sizeButton, i * sizeButton);
+                    this.Controls.Add(button);
+                }
+            }
+        }
+        public void HideCells()
+        {
+            int N = 40;
+            Random random = new Random();
+            while (N > 0)
+            {
+                for (int i = 0; i < n * n; i++)
+                {
+                    for (int j = 0; j < n * n; j++)
+                    {
+                        if (!string.IsNullOrEmpty(buttons[i, j].Text))
+                        {
+                            int a = random.Next(0, 3);
+                            buttons[i, j].Text = a == 0 ? "" : buttons[i, j].Text;
+                            buttons[i, j].Enabled = a == 0 ? true : false;
+
+                            if (buttons[i, j].Enabled == false)
+                            {
+                                buttons[i, j].BackColor = Color.White;
+                                buttons[i, j].ForeColor = Color.Black;
+                            }
+                            else
+                                buttons[i, j].Cursor = Cursors.Hand;
+
+                            if (a == 0)
+                                N--;
+                            if (N <= 0)
+                                break;
+                        }
+                    }
+                    if (N <= 0)
+                        break;
+                }
+            }
+        }
+        public void ShuffleMap(int i)
+        {
+            switch (i)
+            {
+                case 0:
+                    MatrixTransposition();
+                    break;
+                case 1:
+                    SwapRowsInBlock();
+                    break;
+                case 2:
+                    SwapColumnsInBlock();
+                    break;
+                case 3:
+                    SwapBlocksInRow();
+                    break;
+                case 4:
+                    SwapBlocksInColumn();
+                    break;
+                default:
+                    MatrixTransposition();
+                    break;
+            }
+        }
+
+        public void MatrixTransposition()
+        {
+            int[,] tMap = new int[n * n, n * n];
+            for (int i = 0; i < n * n; i++)
+            {
+                for (int j = 0; j < n * n; j++)
+                {
+                    tMap[i, j] = map[j, i];
+                }
+            }
+            map = tMap;
+        }
+        public void SwapRowsInBlock()
+        {
+            Random random = new Random();
+            var block = random.Next(0, n);
+            var row1 = random.Next(0, n);
+            var line1 = block * n + row1;
+            var row2 = random.Next(0, n);
+            while (row1 == row2)
+                row2 = random.Next(0, n);
+            var line2 = block * n + row2;
+            for (int i = 0; i < n * n; i++)
+            {
+                var temp = map[line1, i];
+                map[line1, i] = map[line2, i];
+                map[line2, i] = temp;
+            }
+        }
+        public void SwapColumnsInBlock()
+        {
+            Random random = new Random();
+            var block = random.Next(0, n);
+            var row1 = random.Next(0, n);
+            var line1 = block * n + row1;
+            var row2 = random.Next(0, n);
+            while (row1 == row2)
+                row2 = random.Next(0, n);
+            var line2 = block * n + row2;
+            for (int i = 0; i < n * n; i++)
+            {
+                var temp = map[i, line1];
+                map[i, line1] = map[i, line2];
+                map[i, line2] = temp;
+            }
+        }
+        public void SwapBlocksInRow()
+        {
+            Random random = new Random();
+            var block1 = random.Next(0, n);
+            var block2 = random.Next(0, n);
+            while (block1 == block2)
+                block2 = random.Next(0, n);
+            block1 *= n;
+            block2 *= n;
+            for (int i = 0; i < n * n; i++)
+            {
+                var k = block2;
+                for (int j = block1; j < block1 + n; j++)
+                {
+                    var temp = map[j, i];
+                    map[j, i] = map[k, i];
+                    map[k, i] = temp;
+                    k++;
+                }
+            }
+        }
+        public void SwapBlocksInColumn()
+        {
+            Random random = new Random();
+            var block1 = random.Next(0, n);
+            var block2 = random.Next(0, n);
+            while (block1 == block2)
+                block2 = random.Next(0, n);
+            block1 *= n;
+            block2 *= n;
+            for (int i = 0; i < n * n; i++)
+            {
+                var k = block2;
+                for (int j = block1; j < block1 + n; j++)
+                {
+                    var temp = map[i, j];
+                    map[i, j] = map[i, k];
+                    map[i, k] = temp;
+                    k++;
+                }
+            }
+        }
+        public void OnCellPressed(object sender, EventArgs e)
+        {
+            Button pressedButton = sender as Button;
+            string buttonText = pressedButton.Text;
+            if (string.IsNullOrEmpty(buttonText))
+            {
+                pressedButton.Text = "1";
+            }
+            else
+            {
+                int num = int.Parse(buttonText);
+                num++;
+                if (num == 10)
+                    num = 1;
+                pressedButton.Text = num.ToString();
+            }
+        }
+
+        private void Check()
+        {
+            bool allFilled = this.Controls.OfType<Button>().All(b => !string.IsNullOrWhiteSpace(b.Text));
+            for (int i = 0; i < n * n; i++)
+            {
+                for (int j = 0; j < n * n; j++)
+                {
+                    if (!allFilled)
+                    {
+                        MessageBox.Show("Заполните все поля!");
+                        return;
+                    }
+                    if (buttons[i, j].Text != map[i, j].ToString())
+                    {
+                        MessageBox.Show("Решено НЕПРАВИЛЬНО!");
+                        return;
+                    }
+                }
+            }
+            MessageBox.Show("Решено ПРАВИЛЬНО!");
+        }
+        private void CheckButton_Click(object sender, EventArgs args)
+        {
+            Check();
+        }
+        private void StartOver()
+        {
+            for (int i = 0; i < n * n; i++)
+            {
+                for (int j = 0; j < n * n; j++)
+                {
+                    this.Controls.Remove(buttons[i, j]);
+                }
+            }
+            GenerateMap();
+        }
+        private void StartOverButton_Click(object sender, EventArgs e)
+        {
+            StartOver();
+        }
+    }
+}
